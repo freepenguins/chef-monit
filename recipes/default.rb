650420e4bbe403a67ee77c67be1ef0f83e77411f
James Lyons <james.penguins@gmail.com>
2012-09-05 03:48:41 -0400

    Changes to monit for Centos

--- recipes/default.rb
+++ recipes/default.rb
@@ -15,28 +15,18 @@ service "monit" do
   supports [:start, :restart, :stop]
 end
 
-directory "/etc/monit/conf.d" do
+template "/etc/monit.conf" do
+  owner "root"
+  group "root"
+  mode 0700
+  source 'monitrc.erb'
+  notifies :restart, resources(:service => "monit"), :delayed
+end
+
+directory "/etc/monit.d/" do
   owner  'root'
   group 'root'
   mode 0755
   action :create
   recursive true
-end
-
-template "/etc/monit/monitrc" do
-  owner "root"
-  group "root"
-  mode 0700
-  source 'monitrc.erb'
-  variables(
-    :logfile          => node["monit"]["logfile"],
-    :notify           => node["monit"]["notify"],
-    :httpd            => node["monit"]["httpd"],
-    :poll_period      => node["monit"]["poll_period"],
-    :poll_start_delay => node["monit"]["poll_start_delay"],
-    :mail             => node["monit"]["mail"],
-    :queue            => node["monit"]["queue"],
-    :config_directory => "/etc/monit/conf.d"
-  )
-  notifies :restart, resources(:service => "monit"), :delayed
 end
